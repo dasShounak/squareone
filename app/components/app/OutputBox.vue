8f3e38777d1e53a0d4da26614f838c2b2b712872
Shounak Das <shounakdas@protonmail.com>
2025-10-29 16:17:52 +0000

    feat: Breakdown button components

---
+++ app/components/app/OutputBox.vue
@@ -0,0 +1,27 @@
+<script setup lang="ts">
+import {
+  Item,
+  ItemActions,
+  ItemTitle,
+} from "@/components/ui/item";
+
+const { output } = defineProps<{
+  output: string;
+}>();
+</script>
+
+<template>
+  <Item variant="outline">
+    <div class="flex w-full gap-2">
+      <ItemTitle class="w-full">
+        {{ output }}
+      </ItemTitle>
+      <ItemActions>
+        <AppButtonCopy
+          :copy-text="output"
+          variant="secondary"
+        />
+      </ItemActions>
+    </div>
+  </Item>
+</template>
