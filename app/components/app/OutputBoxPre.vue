96d5ca31318ecbd3e1db3939e913880868bb2e02
Shounak Das <shounakdas@protonmail.com>
2025-12-25 16:35:27 +0000

    feat: Added domain scanning

---
+++ app/components/app/OutputBoxPre.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+import {
+  Item,
+  ItemActions,
+  ItemTitle,
+} from "@/components/ui/item";
+
+const { output } = defineProps<{
+  output: string;
+}>();
+</script>
+
+<template>
+  <Item variant="outline">
+    <div class="flex w-full gap-2">
+      <ItemTitle class="w-full">
+        <pre class="font-sans">{{ output }}</pre>
+      </ItemTitle>
+      <ItemActions>
+        <AppButtonCopy :copy-text="output" variant="secondary" />
+      </ItemActions>
+    </div>
+  </Item>
+</template>
